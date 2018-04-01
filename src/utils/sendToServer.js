1255bf0cdccb50276a90b04fa983ad599ac2fa9c
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-04-02 00:52:33 +0300

    В функции запроса на сервер убрал вывод лога

--- src/utils/sendToServer.js
+++ src/utils/sendToServer.js
@@ -1,21 +1,3 @@
-// Пример обработки события dataIsSent. Рекомендуется изменить API модуля так,
-// чтобы вызова события через document не было
-
-// function test() {
-//     document.addEventListener('dataIsSent', function(event) {
-//         console.log('event got ' + event.detail);
-//     });
-//     sendToServer('mydata');
-// }
-const sendToServer = data => {
-  const response = `Сервер принял данные "${data}".`;
-
-  return new Promise(resolve => {
-    setTimeout(() => {
-      console.log(response);
-      resolve(response);
-    }, 100);
-  });
-};
+const sendToServer = data => new Promise(resolve => setTimeout(() => resolve(data), 100));
 
 export default sendToServer;
